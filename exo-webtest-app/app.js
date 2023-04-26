e774d2fcf87b68e7f4ea714a0c76a853297c3f1d
Denis Arnst <git@sapd.eu>
2023-04-26 13:06:21 +0200

    Added exo-webtest and updated workflow

---
+++ exo-webtest-app/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var os = require('os');
+
+app.get('/', function (req, res) {
+  var body = "<html><head><title>Hello from " + os.hostname() + "</title></head>" +
+    "<body>" +
+    "<img width=250px src=https://www.exoscale.com/static/img/exoscale-logo-full-201711.svg alt=ExoscaleLogo><br>" +
+    "<p>Hello World from host " + os.hostname() + "!</p>" +
+    "</body></html>"
+  res.send(body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
